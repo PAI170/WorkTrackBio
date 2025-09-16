a42937f34f3fcaa9d3adf0d8b7ee90d934884b76
David Rodriguez <admin@rodcastsolutions.com>
2025-09-16 14:53:46 -0600

    WTB_App: Se implemento el uso de lucide angular

--- WorkTrackBio/WorkTrackBio_App/src/app/modules/shared/layouts/main/main.ts
+++ WorkTrackBio/WorkTrackBio_App/src/app/modules/shared/layouts/main/main.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
-import { sharedImports } from '../../../shared/shared';
-
+import { sharedImports } from '../../shared';
+import {House, Search } from 'lucide-angular';
 @Component({
   selector: 'app-main',
-    imports: [
-    ...sharedImports
-  ],
+  standalone: true,
+  imports: [...sharedImports],
   templateUrl: './main.html',
   styleUrl: './main.scss'
 })
 export class Main {
-
+  searchTerm: string='';
+  
+  protected readonly House = House;
+  protected readonly Search = Search;
 }
--- WorkTrackBio/WorkTrackBio_App/src/app/modules/shared/shared.ts
+++ WorkTrackBio/WorkTrackBio_App/src/app/modules/shared/shared.ts
@@ -1,14 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LucideAngularModule, House, Search } from 'lucide-angular';
+import { LucideAngularModule } from 'lucide-angular';
 
 export const sharedImports = [
   CommonModule,
   FormsModule,
   ReactiveFormsModule,
-  LucideAngularModule.pick({ House, Search })
+  LucideAngularModule
+] as const;
+
+export const sharedExports = [
+  CommonModule,
+  FormsModule,
+  ReactiveFormsModule,
+  LucideAngularModule
 ] as const;
 
 export type SharedImports = (typeof sharedImports)[number];
-
-
+export type SharedExports = (typeof sharedExports)[number];
